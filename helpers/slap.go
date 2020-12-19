6329fb33571eebeab4363ff4e3c1542bec8ac1c4
rkrohk <rkakar2000@gmail.com>
2020-12-20 03:38:40 +0530

    Fixed a typo

--- helpers/slap.go
+++ helpers/slap.go
@@ -5,9 +5,9 @@ import (
 	"math/rand"
 )
 
-///Slap takes two users as argument and slaps one
+//Slap takes two users as argument and slaps one
 func Slap(user1 string, user2 string) string {
-	arr := []string{"%s burned to death", "%s hit %s with a chair", "%s hit %s around a bit with a large trout"}
+	arr := []string{"%s burned %s to death", "%s hit %s with a chair", "%s hit %s around a bit with a large trout"}
 	randomIndex := rand.Intn(len(arr))
 	pick := arr[randomIndex]
 	return fmt.Sprintf(pick, user1, user2)
