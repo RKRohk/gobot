42625828617176ffcf8ce62df74b93bf5fc64664
rkrohk <rkakar2000@gmail.com>
2020-12-26 18:54:24 +0530

    Added a slap string

--- helpers/slap.go
+++ helpers/slap.go
@@ -7,7 +7,7 @@ import (
 
 //Slap takes two users as argument and slaps one
 func Slap(user1 string, user2 string) string {
-	arr := []string{"%s burned %s to death", "%s hit %s with a chair", "%s hit %s around a bit with a large trout"}
+	arr := []string{"%s burned %s to death", "%s hit %s with a chair", "%s hit %s around a bit with a large trout", "%s burnt %s to a crisp in backyard bbq"}
 	randomIndex := rand.Intn(len(arr))
 	pick := arr[randomIndex]
 	return fmt.Sprintf(pick, user1, user2)
