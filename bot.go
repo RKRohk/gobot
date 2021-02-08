d6adf185a54595a049530a2f1528457b8a2697a0
rkrohk <rkakar2000@gmail.com>
2021-02-08 22:11:53 +0530

    Added graceful shutdown

--- bot.go
+++ bot.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
 
 	"github.com/rkrohk/gobot/handler"
 
@@ -25,25 +26,41 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
+	go func() {
+		updates, err := bot.GetUpdatesChan(u)
 
-	for update := range updates {
-		if update.Message != nil { // ignore any non-Message Updates
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if err != nil {
+			log.Fatal("There was an error getting updates")
+		}
+		for update := range updates {
+			if update.Message != nil { // ignore any non-Message Updates
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-			if update.Message.Command() != "" {
-				handler.Commandhandler(bot, update)
+				if update.Message.Command() != "" {
+					handler.Commandhandler(bot, update)
+				}
 			}
-		}
 
-		if query := update.CallbackQuery; query != nil {
-			log.Printf("CallbackQuery %s", query.Data)
-			handler.CallbackQueryHandler(bot, &update)
-		}
+			if query := update.CallbackQuery; query != nil {
+				log.Printf("CallbackQuery %s", query.Data)
+				handler.CallbackQueryHandler(bot, &update)
+			}
 
-		if update.InlineQuery != nil {
-			handler.Inlinehandler(bot, &update)
-		}
+			if update.InlineQuery != nil {
+				handler.Inlinehandler(bot, &update)
+			}
 
-	}
+		}
+	}()
+
+	//Added graceful shutdown
+	exitChan := make(chan os.Signal)
+	signal.Notify(exitChan, os.Interrupt)
+	signal.Notify(exitChan, os.Kill)
+
+	s := <-exitChan
+	log.Println("Gracefully Shutting down the bot", s)
+
+	bot.StopReceivingUpdates()
+	return
 }
